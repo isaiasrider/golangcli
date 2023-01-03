1c1a8b742a635b69a7339500f16976d556d6025e
isaiasrider <isaiasonthecloud@gmail.com>
2023-01-02 23:04:48 -0300

    change way of doing tests

---
+++ pragprog.com/rggo/todo/todo.v1/main_test.go
@@ -0,0 +1,62 @@
+package main_test
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"testing"
+)
+
+var (
+	binName  = "todo"
+	fileName = "test.todo.json"
+)
+
+func TestMain(m *testing.M) {
+	fmt.Println("Building Tool...")
+
+	if runtime.GOOS == "windows" {
+		binName += ".exe"
+	}
+	build := exec.Command("go", "build", "-o", binName)
+	if err := build.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Cannot build too %s: %s", binName, err)
+		os.Exit(1)
+	}
+	fmt.Println("Running Tests.....")
+	result := m.Run()
+	fmt.Println("Cleaning Up")
+	os.Remove(binName)
+	os.Remove(fileName)
+	os.Exit(result)
+}
+
+func TestTodoCli(t *testing.T) {
+	task := "task number 1"
+
+	dir, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+	cmdPath := filepath.Join(dir, binName)
+	t.Run("AddNewTask", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, "-task", task)
+		if err := cmd.Run(); err != nil {
+			t.Fatal(err)
+		}
+	})
+	t.Run("ListTasks", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, "-list")
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			t.Fatal(err)
+		}
+		expected := task + "\n"
+		if expected != string(out) {
+			t.Errorf("Expected %q, got %q instead", expected, string(out))
+		}
+		os.Remove("todo.json")
+	})
+}
